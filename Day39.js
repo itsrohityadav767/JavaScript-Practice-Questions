a2bd030458fe07839d799a9cf4ecc27ed820eaa0
rohityadav <yyadavrrohit767@gmail.com>
2025-02-16 22:45:42 +0530

    Calculate Age

---
+++ Day39.js
@@ -0,0 +1,15 @@
+function calculateAge(birthDate){
+    let todayDate = new Date();
+
+    birthDate = new Date(birthDate);
+
+    let age = todayDate.getFullYear()-birthDate.getFullYear();
+    let monthDiff = todayDate.getMonth() - birthDate.getMonth();
+
+   if(monthDiff <0 || (monthDiff == 0 && todayDate.getDate()<birthDate.getDate())){
+    age--;
+   }
+   return age;
+}
+
+console.log(calculateAge("1990-05-15"));
